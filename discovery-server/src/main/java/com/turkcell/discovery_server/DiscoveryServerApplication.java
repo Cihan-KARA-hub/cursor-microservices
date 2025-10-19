cbc9ef9e790e9ffb645c6249aaae45f60c857d68
Cihan-KARA-hub <cihankara632@gmail.com>
2025-10-19 11:05:25 +0300

    feat : discovery-server impl

---
+++ discovery-server/src/main/java/com/turkcell/discovery_server/DiscoveryServerApplication.java
@@ -0,0 +1,14 @@
+package com.turkcell.discovery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
+}
